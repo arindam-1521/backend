e52d340c059811e41059cac1afc9f40de238995e
arindam <arindampradhan562@gmail.com>
2023-03-25 13:41:45 +0530

    dj

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use("/api/auth", require("./routes/auth"))
 app.use("/api/notes", require("./routes/notes"))
 
-app.get("/", (req, res) => {
+app.get("/details", (req, res) => {
     res.send("Hello")
 })
 
