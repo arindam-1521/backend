fa3674f8d44fe4b43096e574901b86c92383b050
arindam <arindampradhan562@gmail.com>
2023-03-27 01:35:51 +0530

    commited all changes successfully

--- routes/auth.js
+++ routes/auth.js
@@ -10,32 +10,67 @@ const fetchuser = require("../middleware/fetchuser")
 
 //?Route-1 Create a User Using POST "/api/auth/createuser" Doesn't Require Auth.
 router.post("/createuser", [body('email', "Enter a valid Email.").isEmail(), body('password', "Enter A Valid PassWord").isLength({ min: 5 }), body('name', "Enter a valie Name").isLength({ min: 3 })], async (req, res) => {
+    let success = false;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+        return res.status(400).json({ errors: errors.array(), success, error: "Check the Credentials You Entered." });
     }
 
 
-    const salt = await bcrypt.genSalt(10)
-    let secPass = await bcrypt.hash(req.body.password, salt)
-    await User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: secPass
-    }).then(user => {
+
+    /*    const salt = await bcrypt.genSalt(10)
+        let secPass = await bcrypt.hash(req.body.password, salt)
+        await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: secPass
+        }).then(user => {
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+            const authToken = jwt.sign(data, JWT_SECRET)
+            console.log(authToken)
+            res.json({ authToken })
+            console.log("Saved to Db")
+        }).catch(err => {
+            console.log("Failed To Save to The DataBase." + (err.message))
+            res.status(500).send("Internal Server Error Some Error Occured.")
+        });*/
+    try {
+        // Check whether the user with this email exists already
+        let user = await User.findOne({ email: req.body.email });
+        if (user) {
+            return res.status(400).json({ success, error: "Sorry a user with this email already exists" })
+        }
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password, salt);
+
+        // Create a new user
+        user = await User.create({
+            name: req.body.name,
+            password: secPass,
+            email: req.body.email,
+        });
         const data = {
             user: {
                 id: user.id
             }
         }
-        const authToken = jwt.sign(data, JWT_SECRET)
-        console.log(authToken)
-        res.json({ authToken })
-        console.log("Saved to Db")
-    }).catch(err => {
-        console.log("Failed To Save to The DataBase." + (err.message))
-        res.status(500).send("Internal Server Error Some Error Occured.")
-    });
+        const authToken = jwt.sign(data, JWT_SECRET);
+
+
+        // res.json(user)
+        success = true;
+        res.json({ success, authToken })
+
+    }
+    catch (error) {
+        console.error(error.message);
+        res.status(500).send({ success, error: "Internal Server Error" });
+    }
+
 })
 
 //* Route-2 Authentication a user using POST "api/auth/login" , No Login required.
@@ -43,6 +78,7 @@ router.post("/createuser", [body('email', "Enter a valid Email.").isEmail(), bod
 router.post('/login', [body('email', "Enter a valid Email.").isEmail(), body('password', "PassWord Can't be blank.").exists()], async (req, res) => {
     //If there are erros then they have to be sent.
     const errors = validationResult(req)
+    let success = false;
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
@@ -52,23 +88,25 @@ router.post('/login', [body('email', "Enter a valid Email.").isEmail(), body('pa
     try {
         let user = await User.findOne({ email })
         if (!user) {
-            return res.status(400).json({ error: "Please try to login with correct credentials." })
+            return res.status(400).json({ success, error: "Please try to login with correct credentials." })
         }
 
         const passwordCompare = await bcrypt.compare(password, user.password)
         if (!passwordCompare) {
-            return res.status(400).json({ error: "Please try to login with correct credentials." })
+            success = false
+            return res.status(400).json({ success, error: "Please try to login with correct credentials." })
         }
         const data = {
             user: {
                 id: user.id
             }
         }
+        success = true;
         const authToken = jwt.sign(data, JWT_SECRET)
-        res.json(authToken)
+        res.json({ success, authToken })
     } catch (err) {
         console.log("Failed To Save to The DataBase." + (err.message))
-        res.status(500).send("Internal Server Error Some Error Occured.")
+        res.status(500).json({ success, error: "Internal Server Error Some Error Occured." })
     }
 
 })
